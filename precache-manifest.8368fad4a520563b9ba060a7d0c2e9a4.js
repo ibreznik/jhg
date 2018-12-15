a007ef9505655b3f41e79a0580986c0036e9148c
Ilya Reznik <reznik@adobe.com>
2018-12-14 22:24:29 -0700

    Updates

---
+++ precache-manifest.8368fad4a520563b9ba060a7d0c2e9a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c888766c797283357ec",
+    "url": "/jhg/static/css/main.f4421f75.chunk.css"
+  },
+  {
+    "revision": "8c888766c797283357ec",
+    "url": "/jhg/static/js/main.8c888766.chunk.js"
+  },
+  {
+    "revision": "8e23cdf8113cc797e7ac",
+    "url": "/jhg/static/js/1.8e23cdf8.chunk.js"
+  },
+  {
+    "revision": "475ddc6de4cdb6e0ef5a",
+    "url": "/jhg/static/js/runtime~main.475ddc6d.js"
+  },
+  {
+    "revision": "0c1ce2ddc2098fdaf454684a334fba15",
+    "url": "/jhg/static/media/play.0c1ce2dd.svg"
+  },
+  {
+    "revision": "5e8e3bc43a607ff8811cc2598a40ca96",
+    "url": "/jhg/static/media/check_blank.5e8e3bc4.svg"
+  },
+  {
+    "revision": "07199ac9c29c8cec063a0590245d35c5",
+    "url": "/jhg/static/media/check_full.07199ac9.svg"
+  },
+  {
+    "revision": "586f15b33ad23ad887c314c41fd8d1d3",
+    "url": "/jhg/static/media/pause.586f15b3.svg"
+  },
+  {
+    "revision": "4b3f33aa6d78cd274833cd8755588d2b",
+    "url": "/jhg/static/media/plus.4b3f33aa.svg"
+  },
+  {
+    "revision": "bc3353e317f9ee36a03a8d6478b4b111",
+    "url": "/jhg/index.html"
+  }
+];
